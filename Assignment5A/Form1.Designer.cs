82234d1d61995e93456330517e4e0e5c84199bad
Mohammed Faisal Ansari <49450406+Ansarifaisala@users.noreply.github.com>
2022-01-03 23:00:59 -0500

    Add files via upload

---
+++ Assignment5A/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Assignment5A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TapPictureBox = new System.Windows.Forms.PictureBox();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.speedTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.TapPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TapPictureBox
+            // 
+            this.TapPictureBox.ErrorImage = global::Assignment5A.Properties.Resources.Faucet;
+            this.TapPictureBox.Image = global::Assignment5A.Properties.Resources.Faucet;
+            this.TapPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("TapPictureBox.InitialImage")));
+            this.TapPictureBox.Location = new System.Drawing.Point(12, 148);
+            this.TapPictureBox.Name = "TapPictureBox";
+            this.TapPictureBox.Size = new System.Drawing.Size(92, 71);
+            this.TapPictureBox.TabIndex = 0;
+            this.TapPictureBox.TabStop = false;
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Location = new System.Drawing.Point(12, 77);
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(188, 45);
+            this.speedTrackBar.TabIndex = 1;
+            this.speedTrackBar.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(12, 13);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(75, 23);
+            this.colorButton.TabIndex = 2;
+            this.colorButton.Text = "Select Color";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(93, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // speedTimer
+            // 
+            this.speedTimer.Tick += new System.EventHandler(this.waterDrop);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.TapPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TapPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox TapPictureBox;
+        private System.Windows.Forms.TrackBar speedTrackBar;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Timer speedTimer;
+    }
+}
+
---
+++ Assignment5A/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+//@author Ansari Mohammed Faisal - 000812671
+
+namespace Assignment5A
+{
+    /// <summary>
+    /// class creates a apps which displays water flowing and filling the bucket
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        Color waterColour = Color.Blue; //default color
+        private int lengthCounter = 1; //keep track of water length
+        bool bucketFull = false;
+        public Form1()
+        {
+            InitializeComponent();
+            base.Paint += bucketDrawer; //draw bucket
+            speedTrackBar.Value = 0;
+        }
+
+        /// <summary>
+        /// this function is called when user chooses a color for water by clicking colour button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colourChoosed = new ColorDialog();
+            if (colourChoosed.ShowDialog() == DialogResult.OK)
+            {
+                waterColour = colourChoosed.Color;
+            }
+        }
+        /// <summary>
+        /// this function is used to display water that flows from the tap
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void waterDrop(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            SolidBrush brush = new SolidBrush(waterColour);
+            graphics.FillRectangle(brush, 85, 200, 15, 201 - lengthCounter);
+            if (this.lengthCounter >= 100)
+            {
+                bucketFull = true;
+                speedTimer.Enabled = false;
+                speedTrackBar.Value = 0;
+                this.lengthCounter = 0;
+                graphics.FillRectangle(new SolidBrush(BackColor), 85, 200, 15, 101);
+            }
+            graphics.FillRectangle(brush, 86, 400 - this.lengthCounter, 199, 1);
+            this.lengthCounter++;
+        }
+
+        /// <summary>
+        /// this function closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        /// <summary>
+        /// this function draws bucket
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="paintEvent"></param>
+        private void bucketDrawer(object a, PaintEventArgs paintEvent)
+        {
+            Graphics graphics = paintEvent.Graphics;
+            graphics.DrawLine(new Pen(Color.Black), 84, 290, 84, 400);
+            graphics.DrawLine(new Pen(Color.Black), 84, 400, 285, 400);
+            graphics.DrawLine(new Pen(Color.Black), 285, 400, 285, 290);
+
+        }
+
+        /// <summary>
+        /// this function controls the speed as well as fill the bucket with water(rectangle)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            var speed = (speedTrackBar.Maximum - speedTrackBar.Value) * 10;
+            if (speedTrackBar.Value == 10)//prevent 0 speed when max value
+            {
+                speed = (int)(0.5 * 10);
+            }
+            Graphics graphics = CreateGraphics();
+            if (speedTrackBar.Value == 0)
+            {
+                speedTimer.Enabled = false;
+
+            }
+            else
+            {
+                if (!bucketFull)
+                {
+                    speedTimer.Interval = speed;
+                    speedTimer.Enabled = true;
+                }
+                else
+                {
+                    bucketFull = false;
+                    graphics.FillRectangle(new SolidBrush(BackColor), 86, 285, 199, 140);
+                    graphics.FillRectangle(new SolidBrush(BackColor), 86, 200, 15, 86);
+                    speedTimer.Interval = speed;
+                    speedTimer.Enabled = true;
+                }
+            }
+        }
+    }
+}
